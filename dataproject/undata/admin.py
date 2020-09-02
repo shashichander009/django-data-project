d94d72785a1092e61b1d08577fd3588bd5eaf2a3
Shashi Kumar Sah <shashichander009@gmail.com>
2020-09-02 14:46:36 +0530

    directory change

--- dataproject/undata/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import RegionData, Union
-
-admin.site.register(RegionData)
-admin.site.register(Union)
--- dataproject/undata/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UndataConfig(AppConfig):
-    name = 'undata'
